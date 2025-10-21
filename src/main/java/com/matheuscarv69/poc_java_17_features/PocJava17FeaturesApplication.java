04af70ebcb28a46c997bb796075d55f982304a70
matheuscarv69 <matheus9126@gmail.com>
2025-10-21 14:53:34 -0400

    :tada: initial commit

---
+++ src/main/java/com/matheuscarv69/poc_java_17_features/PocJava17FeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.poc_java_17_features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJava17FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJava17FeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscarv69/poc_java_17_features/PocJava17FeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscarv69.poc_java_17_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJava17FeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
