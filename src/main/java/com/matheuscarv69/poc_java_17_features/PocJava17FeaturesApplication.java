1333cd681f2f85964dd9e052d95deb30e97eacdd
matheuscarv69 <matheus9126@gmail.com>
2025-10-21 18:25:05 -0400

    :fire: refactor: remove Spring Boot dependencies and simplify main application class

--- src/main/java/com/matheuscarv69/poc_java_17_features/PocJava17FeaturesApplication.java
+++ src/main/java/com/matheuscarv69/poc_java_17_features/PocJava17FeaturesApplication.java
@@ -1,13 +1,8 @@
 package com.matheuscarv69.poc_java_17_features;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class PocJava17FeaturesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocJava17FeaturesApplication.class, args);
-	}
+    public static void main(String[] args) {
+    }
 
 }
